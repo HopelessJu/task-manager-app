5bf83c1c38190543e7044c09b0c4e41c42e1084a
Aleksandra <s.tsikhmanovich@gmail.com>
2023-03-22 10:42:27 +0100

    feat: edit routing

--- task-manager-app/src/app/app-routing.module.ts
+++ task-manager-app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { SignupComponent } from './authentication/components/signup/signup.compo
 import { BoardsComponent } from './boards/pages/main-page/boards.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleBoardPageComponent } from './single-board/pages/single-board-page/single-board-page.component';
 
 const routes: Routes = [
   { path: 'main', component: BoardsComponent },
   { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'board', component: SingleBoardPageComponent}
 ];
 
 @NgModule({
